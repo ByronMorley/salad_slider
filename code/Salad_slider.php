2ce71b7166f2ae7f9bdd960c1e4b3f900656cc15
Byron <byron@atebol.com>
2018-05-18 12:01:58 +0100

    first commit

---
+++ code/Salad_slider.php
@@ -0,0 +1,64 @@
+<?php
+
+class Salad_slider extends DataExtension
+{
+
+	public static $allowed_actions = array();
+
+	private static $db = array();
+
+	private static $has_one = array();
+
+	private static $has_many = array(
+		'Widgets' => 'Widget'
+	);
+
+	public function contentControllerInit()
+	{
+		Requirements::css(BUILDING_BLOCKS_DIR . '/css/style.css');;
+		Requirements::javascript(BUILDING_BLOCKS_DIR . '/js/main.min.js');
+	}
+
+	public function getCMSFields()
+	{
+		$fields = parent::getCMSFields();
+		$this->extend('updateCMSFields', $fields);
+		return $fields;
+	}
+
+	public function updateCMSFields(FieldList $fields)
+	{
+
+		/*********************************
+		 *      COMPONENT BUILDER
+		 ********************************/
+
+		$dataColumns = new GridFieldDataColumns();
+		$dataColumns->setDisplayFields(
+			array(
+				'ClassName' => 'Class Name'
+			)
+		);
+
+		$multiClassConfig = new GridFieldAddNewMultiClass();
+		$multiClassConfig->setClasses(
+			array(
+				'WidgetFancyBanner' => WidgetFancyBanner::get_widget_type(),
+			)
+		);
+
+		$config = GridFieldConfig_RelationEditor::create()
+			->removeComponentsByType('GridFieldAddNewButton')
+			->addComponents(
+				new GridFieldOrderableRows('SortOrder'),
+				new GridFieldDeleteAction(),
+				$multiClassConfig,
+				$dataColumns
+			);
+
+		$gridField = GridField::create('Widgets', "Widgets", $this->owner->Widgets(), $config);
+		$fields->addFieldToTab("Root.Widgets", $gridField);
+
+
+	}
+}
---
+++ scripts/refineSlide/refineSlideInit.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+});
+
+function refinSlideInit() {
+    return {
+        onInit: function () {
+            this.slider.$currentSlide.find('img').addClass('kenburns');
+
+            var _this = this.slider;
+
+            $('.rs-link').on('click', function () {
+                var cl = $(this).data('class').split('-')[3];
+                _this.transition(cl - 1);
+            });
+        },
+        onChange: function () {
+            this.slider.$nextSlide.find('img').addClass('kenburns');
+
+        },
+        afterChange: function () {
+            this.slider.$previousSlide.find('img').removeClass('kenburns');
+        }
+    };
+}
+function rsSlide() {
+    $('#rs-1').refineSlide(refinSlideInit());
+}
